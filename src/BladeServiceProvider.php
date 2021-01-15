713d89bfd5c98c83f91d9243e74ff23c931ce7a0
Johan Sjöland <johan@sjoland.com>
2021-01-15 23:56:35 +0100

    Blade directives

--- src/BladeServiceProvider.php
+++ src/BladeServiceProvider.php
@@ -8,7 +8,17 @@ use Illuminate\Support\Facades\Blade;
 class BladeServiceProvider extends ServiceProvider {
 
     public function boot() {
-		//
+
+		// Lipsum
+        Blade::directive('lipsum', function($expression){
+			return "<?php echo _lipsum($expression); ?>";
+        });
+
+		// Pixsum
+        Blade::directive('pixsum', function($expression){
+			return "<?php echo _pixsum($expression); ?>";
+        });
+
     }
 
     public function register() {
--- src/helpers.php
+++ src/helpers.php
@@ -1,3 +1,19 @@
 <?php
 
 // ---
+
+if (!function_exists('_lipsum')) {
+
+	function _lipsum() {
+		return \ABetter\Mockup\Lipsum::get(...func_get_args());
+	}
+
+}
+
+if (!function_exists('_pixsum')) {
+
+	function _pixsum() {
+		return \ABetter\Mockup\Pixsum::get(...func_get_args());
+	}
+
+}
